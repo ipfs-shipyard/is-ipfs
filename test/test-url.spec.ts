bc9cd3ecb875aa23a5d3dd1612822fe09ef8b382
Alex Potsides <alex@achingbrain.net>
2022-09-21 17:16:02 +0100

    feat!: convert to typescript (#53)
    BREAKING CHANGE: this module is now ESM only

--- test/test-url.spec.ts
+++ test/test-url.spec.ts
@@ -1,9 +1,8 @@
 /* eslint-env mocha */
-'use strict'
 
-const { expect } = require('aegir/utils/chai')
-const isIPFS = require('../src/index')
-const { fromString: uint8ArrayFromString } = require('uint8arrays/from-string')
+import { expect } from 'aegir/chai'
+import * as isIPFS from '../src/index.js'
+import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
 
 describe('ipfs url', () => {
   it('isIPFS.ipfsUrl should match an ipfs url', (done) => {
